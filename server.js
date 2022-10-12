67155ee19b47263d12c53f17bd3c9b90f31fe5c3
Krissy Macdonald <krissymacdonald5@gmail.com>
2022-10-12 00:55:38 -0400

    'database' setup

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const drinks = require('./models/drinks')
 
 app.get('/', (req, res) => {
     res.send("Welcome to the GitPub App!")
+})
+
+app.get('/drinks', (req, res) => {
+    // res.send(drinks)
+    console.log(drinks)
 })
 
 app.listen(port, (req, res) => {
