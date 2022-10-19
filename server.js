bc1bcc71f9704fd80dd1eeb6880eb81f7a7723a2
Krissy Macdonald <krissymacdonald5@gmail.com>
2022-10-18 22:20:34 -0400

    food added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const drinks = require('./models/drinks')
+const food = require('./models/food')
 
 app.get('/', (req, res) => {
     res.send("Welcome to the GitPub App!")
@@ -20,6 +21,20 @@ app.get('/drinks/:id', (req, res) => {
         drink: drinks[req.params.id]
     })
     
+})
+
+// Food Index Route
+app.get('/food', (req, res) => {
+    res.render('food_index.ejs', {
+        meals: food,
+    }
+})
+
+// Food Show Route
+app.get('/food/:id', (req, res) => {
+    res.render('food_show.ejs', {
+        meal: food[req.params.id]
+    })
 })
 
 app.listen(port, (req, res) => {
